b1d7933a165bb770b5948559c172ea35ab1e72f9
brent <brent-matthys@outlook.com>
2023-08-26 09:55:01 +0200

    MainLayout: back to top button

--- web/src/components/MainLayout.js
+++ web/src/components/MainLayout.js
@@ -1,5 +1,37 @@
 import {Outlet, Link} from "react-router-dom";
 import NavBar from "./NavBar";
+import {Box, Fab, Fade, Slide, useScrollTrigger} from "@mui/material";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+
+function ScrollTopBtn() {
+    const trigger = useScrollTrigger({
+        //target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    return (
+        <Slide direction="up" in={trigger}>
+            <Box
+                role="presentation"
+                sx={{ position: 'fixed', bottom: 16, right: 16 }}
+            >
+                <Fab size="small" onClick={handleClick}>
+                    <KeyboardArrowUpIcon/>
+                </Fab>
+            </Box>
+        </Slide>
+    );
+}
+
 
 function MainLayout(){
     return(
@@ -20,6 +52,7 @@ function MainLayout(){
             </nav>
 
             <Outlet />
+            <ScrollTopBtn />
         </>
     );
 }
--- web/src/components/NavBar.js
+++ web/src/components/NavBar.js
@@ -3,9 +3,6 @@ import {AppBar, Slide, useScrollTrigger} from '@mui/material';
 
 function HideOnScroll(props) {
     const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
     const trigger = useScrollTrigger({
         target: window ? window() : undefined,
     });
