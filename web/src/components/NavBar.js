aee717322bfcd6ec77e325d8910cddccc4c67451
brent <brent-matthys@outlook.com>
2023-08-26 11:33:15 +0200

    Navbar: Add name to navbar

--- web/src/components/NavBar.js
+++ web/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Slide, Tab, Tabs, useScrollTrigger} from '@mui/material';
+import {AppBar, Box, Grid, Slide, Tab, Tabs, Typography, useScrollTrigger} from '@mui/material';
 import {useLocation} from "react-router-dom";
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
@@ -32,6 +32,7 @@ function LinkTab(props) {
     return (
         <Tab
             component="a"
+            sx={{ mb: 0}}
             {...props}
         />
     );
@@ -42,29 +43,43 @@ function NavBar(){
     return(
         <HideOnScroll>
             <AppBar>
-                <Tabs value={location.pathname}>
-                    <LinkTab
-                        label="Home"
-                        value="/"
-                        href="/"
-                        icon={<HomeIcon />}
-                        iconPosition={'start'}
-                    />
-                    <LinkTab
-                        label="Projects"
-                        value="/projects"
-                        href="/projects"
-                        icon={<WorkIcon />}
-                        iconPosition={'start'}
-                    />
-                    <LinkTab
-                        label="About"
-                        value="/about"
-                        href="/about"
-                        icon={<InfoIcon />}
-                        iconPosition={'start'}
-                    />
-                </Tabs>
+                <Grid container >
+                    <Grid item xs sx={{ textAlign: 'justify-center', mx: 2, mt:2 }}>
+                        <Typography gutterBottom variant="h4" component="div" sx={{ mb: 0}}>
+                            <Box sx={{ fontWeight: 'bold', letterSpacing: 2}}>Tijmen Matthys</Box>
+                        </Typography>
+                        <Typography variant="subtitle1"  component="div" sx={{ mb: 0, letterSpacing: 1}}>
+                            Gamedev student & Songwriter
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Tabs value={location.pathname}>
+                            <LinkTab
+                                label="Home"
+                                value="/"
+                                href="/"
+                                icon={<HomeIcon />}
+                                iconPosition={'start'}
+                            />
+                            <LinkTab
+                                label="Projects"
+                                value="/projects"
+                                href="/projects"
+                                icon={<WorkIcon />}
+                                iconPosition={'start'}
+                            />
+                            <LinkTab
+                                label="About"
+                                value="/about"
+                                href="/about"
+                                icon={<InfoIcon />}
+                                iconPosition={'start'}
+                            />
+                        </Tabs>
+                    </Grid>
+                </Grid>
+
+
             </AppBar>
         </HideOnScroll>
 
