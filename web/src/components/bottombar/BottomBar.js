4d0f2345ccf143744491ec467aab568e272f46a9
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-09-11 10:49:07 +0200

    Bottombar: add icons

--- web/src/components/bottombar/BottomBar.js
+++ web/src/components/bottombar/BottomBar.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import GithubIcon from '../icons/GithubIcon'
 import IconLink from './IconLink';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import SpotifyIcon from '../icons/SpotifyIcon';
+import ItchIoIcon from '../icons/ItchIoIcon';
+import ArtStationIcon from '../icons/ArtStationIcon';
+import SketchfabIcon from '../icons/SketchFabIcon';
+import WolrdsBeyondIcon from '../icons/WorldsBeyondIcon';
 
 function LeftSection(){
   return(
-    <>
+    <Box>
     <IconLink
       name='Github'
-      icon={() => <GithubIcon/>}
+      icon={() => <GitHubIcon/>}
       href='https://github.com/'
     />
-    </>
+    <IconLink
+      name='LinkedIn'
+      icon={() => <LinkedInIcon/>}
+      href='https://github.com/'
+    />
+    <IconLink
+      name='Pinterest'
+      icon={() => <PinterestIcon/>}
+      href='https://github.com/'
+    />
+    <IconLink
+      name='Itch.io'
+      icon={() => <ItchIoIcon/>}
+      href='https://github.com/'
+    />
+    <IconLink
+      name='Artstation'
+      icon={() => <ArtStationIcon/>}
+      href='https://github.com/'
+    />
+    <IconLink
+      name='Sketchfab'
+      icon={() => <SketchfabIcon/>}
+      href='https://github.com/'
+    />
+    <IconLink
+      name='Worlds Beyond'
+      icon={() => <WolrdsBeyondIcon/>}
+      href='https://github.com/'
+    />
+    <IconLink
+      name='YouTube'
+      icon={() => <YouTubeIcon/>}
+      href='https://github.com/'
+    />
+    <IconLink
+      name='Spotify'
+      icon={() => <SpotifyIcon/>}
+      href='https://github.com/'
+    />
+    
+    </Box>
   );
 }
 
--- web/src/components/bottombar/IconLink.js
+++ web/src/components/bottombar/IconLink.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Typography, Box, Link } from "@mui/material";
 
 function IconLink({ name, icon, href }){
-    console.log(name)
     return(
-        <Link href={href} color="primary.contrastText">
+        <Link 
+            href={href}
+            color="primary.contrastText"
+        >
             <Box display="flex" >
                 {icon()}
-                <Typography sx={{ ml: 1 }} >
+                <Typography sx={{ ml:1 }} >
                 {name}
                 </Typography>
             </Box>
