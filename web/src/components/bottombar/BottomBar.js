f67a03537bbf6c860ca3d2d3c2097de18a039e49
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-09-11 10:56:39 +0200

    bottombar: link to correct websites

--- web/src/components/bottombar/BottomBar.js
+++ web/src/components/bottombar/BottomBar.js
@@ -14,50 +14,53 @@ import WolrdsBeyondIcon from '../icons/WorldsBeyondIcon';
 function LeftSection(){
   return(
     <Box>
+     <IconLink
+      name='Itch.io'
+      icon={() => <ItchIoIcon/>}
+      href='https://tijmenmatthys.itch.io/'
+    />
     <IconLink
       name='Github'
       icon={() => <GitHubIcon/>}
-      href='https://github.com/'
+      href='https://github.com/tijmenmatthys'
     />
     <IconLink
       name='LinkedIn'
       icon={() => <LinkedInIcon/>}
-      href='https://github.com/'
-    />
-    <IconLink
-      name='Pinterest'
-      icon={() => <PinterestIcon/>}
-      href='https://github.com/'
-    />
-    <IconLink
-      name='Itch.io'
-      icon={() => <ItchIoIcon/>}
-      href='https://github.com/'
+      href='https://www.linkedin.com/in/tijmenmatthys'
     />
     <IconLink
       name='Artstation'
       icon={() => <ArtStationIcon/>}
-      href='https://github.com/'
+      href='https://www.artstation.com/tijmenmatthys'
     />
     <IconLink
       name='Sketchfab'
       icon={() => <SketchfabIcon/>}
-      href='https://github.com/'
+      href='https://sketchfab.com/tijmenmatthys'
     />
+    <IconLink
+      name='Pinterest'
+      icon={() => <PinterestIcon/>}
+      href='https://www.pinterest.com/tijmenmatthys/'
+    />
+
+
+
     <IconLink
       name='Worlds Beyond'
       icon={() => <WolrdsBeyondIcon/>}
-      href='https://github.com/'
+      href='https://www.worldsbeyond.be/'
     />
     <IconLink
       name='YouTube'
       icon={() => <YouTubeIcon/>}
-      href='https://github.com/'
+      href='https://www.youtube.com/c/worldsbeyond/'
     />
     <IconLink
       name='Spotify'
       icon={() => <SpotifyIcon/>}
-      href='https://github.com/'
+      href='https://open.spotify.com/artist/5Gjhr3kpK4L3NeqVf6GQrw'
     />
     
     </Box>
