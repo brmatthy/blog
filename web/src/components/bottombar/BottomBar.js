cbbc3488f0530d6250a1668a672ffdd08e6cd7e4
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-09-11 11:02:57 +0200

    bottombar: style left section

--- web/src/components/bottombar/BottomBar.js
+++ web/src/components/bottombar/BottomBar.js
@@ -13,55 +13,58 @@ import WolrdsBeyondIcon from '../icons/WorldsBeyondIcon';
 
 function LeftSection(){
   return(
-    <Box>
-     <IconLink
-      name='Itch.io'
-      icon={() => <ItchIoIcon/>}
-      href='https://tijmenmatthys.itch.io/'
-    />
-    <IconLink
-      name='Github'
-      icon={() => <GitHubIcon/>}
-      href='https://github.com/tijmenmatthys'
-    />
-    <IconLink
-      name='LinkedIn'
-      icon={() => <LinkedInIcon/>}
-      href='https://www.linkedin.com/in/tijmenmatthys'
-    />
-    <IconLink
-      name='Artstation'
-      icon={() => <ArtStationIcon/>}
-      href='https://www.artstation.com/tijmenmatthys'
-    />
-    <IconLink
-      name='Sketchfab'
-      icon={() => <SketchfabIcon/>}
-      href='https://sketchfab.com/tijmenmatthys'
-    />
-    <IconLink
-      name='Pinterest'
-      icon={() => <PinterestIcon/>}
-      href='https://www.pinterest.com/tijmenmatthys/'
-    />
+    <Box sx= {{ py: 5, pl: 3}}>
+      <Box sx={{ mb: 3 }} >
+
+        <IconLink
+          name='Itch.io'
+          icon={() => <ItchIoIcon/>}
+          href='https://tijmenmatthys.itch.io/'
+        />
+        <IconLink
+          name='Github'
+          icon={() => <GitHubIcon/>}
+          href='https://github.com/tijmenmatthys'
+        />
+        <IconLink
+          name='LinkedIn'
+          icon={() => <LinkedInIcon/>}
+          href='https://www.linkedin.com/in/tijmenmatthys'
+        />
+        <IconLink
+          name='Artstation'
+          icon={() => <ArtStationIcon/>}
+          href='https://www.artstation.com/tijmenmatthys'
+        />
+        <IconLink
+          name='Sketchfab'
+          icon={() => <SketchfabIcon/>}
+          href='https://sketchfab.com/tijmenmatthys'
+        />
+        <IconLink
+          name='Pinterest'
+          icon={() => <PinterestIcon/>}
+          href='https://www.pinterest.com/tijmenmatthys/'
+        />
+      </Box>
 
 
 
-    <IconLink
-      name='Worlds Beyond'
-      icon={() => <WolrdsBeyondIcon/>}
-      href='https://www.worldsbeyond.be/'
-    />
-    <IconLink
-      name='YouTube'
-      icon={() => <YouTubeIcon/>}
-      href='https://www.youtube.com/c/worldsbeyond/'
-    />
-    <IconLink
-      name='Spotify'
-      icon={() => <SpotifyIcon/>}
-      href='https://open.spotify.com/artist/5Gjhr3kpK4L3NeqVf6GQrw'
-    />
+      <IconLink
+        name='Worlds Beyond'
+        icon={() => <WolrdsBeyondIcon/>}
+        href='https://www.worldsbeyond.be/'
+      />
+      <IconLink
+        name='YouTube'
+        icon={() => <YouTubeIcon/>}
+        href='https://www.youtube.com/c/worldsbeyond/'
+      />
+      <IconLink
+        name='Spotify'
+        icon={() => <SpotifyIcon/>}
+        href='https://open.spotify.com/artist/5Gjhr3kpK4L3NeqVf6GQrw'
+      />
     
     </Box>
   );
