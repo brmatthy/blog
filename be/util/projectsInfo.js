c8307d0d204c649f6dff3ba9e2b3ebdc5362c162
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-09-16 09:55:48 +0200

    Backend: md file to html for api

--- be/util/projectsInfo.js
+++ be/util/projectsInfo.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const showdown = require('showdown');
 
 function getProjectsDir(){
     return path.join(__dirname, '..', 'public', 'projects');
@@ -45,7 +46,38 @@ function getProjectUrls(req){
     const json = {
         "meta": `${baseUrl}/projects/${projectName}/meta.json`,
         "thumbnail": `${baseUrl}/projects/${projectName}/thumbnail.jpg`,
-        "page": `${baseUrl}/projects/${projectName}/page.md`
+        "page": `${baseUrl}/${projectName}/page`
+    }
+    return json;
+}
+
+function pageToHTML(mdPath){
+    // get the md file as a string
+    let md;
+    try{
+        md = fs.readFileSync(mdPath, 'utf-8');
+    }catch{
+        return null;
+    }
+
+    // convert to html
+    const converter = new showdown.Converter();
+    const html = converter.makeHtml(md);
+
+    return html;
+}
+
+function getProjectHTML(req){
+    const projectName = req.params.projectName;
+    if(!projectFolderExists(projectName)){
+        return null;
+    }
+    const baseUrl = getBaseUrl(req);
+    const mdPath = path.join(getProjectsDir(), projectName, 'page.md');
+    let html = pageToHTML(mdPath);
+    const json = {
+        "project": `${baseUrl}/${projectName}`,
+        "page": html
     }
     return json;
 }
@@ -54,4 +86,5 @@ function getProjectUrls(req){
 module.exports = {
     getAllProjectUrls,
     getProjectUrls,
+    getProjectHTML,
 }
